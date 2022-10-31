9f8eea1467bbdb609225812f8567221142b266dd
Lê Quang Trọng Tài <iletai@hotmail.com>
2022-10-31 10:35:46 +0700

    + Add some feature for View SwiftUI

--- Sources/SwiftUICommon/CustomStyle/ViewCustom.swift
+++ Sources/SwiftUICommon/CustomStyle/ViewCustom.swift
@@ -12,4 +12,67 @@ extension View {
     public func widthInfinity() -> some View {
         self.modifier(CustomViewModifier.Infinity())
     }
+
+    func debugAction(_ closure: () -> Void) -> Self {
+#if DEBUG
+        closure()
+#endif
+
+        return self
+    }
+
+    func debugPrint(_ value: Any) -> Self {
+        debugAction { print(value) }
+    }
+
+    func debugModifier<T: View>(_ modifier: (Self) -> T) -> some View {
+#if DEBUG
+        return modifier(self)
+#else
+        return self
+#endif
+    }
+
+    func debugBorder(_ color: Color = .red, width: CGFloat = 1) -> some View {
+        debugModifier {
+            $0.border(color, width: width)
+        }
+    }
+
+    func debugBackground(_ color: Color = .red) -> some View {
+        debugModifier {
+            $0.background(color)
+        }
+    }
+
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content
+    ) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+
+    func shadowedStyle() -> some View {
+        self
+            .shadow(color: .black.opacity(0.08), radius: 2, x: 0, y: 0)
+            .shadow(color: .black.opacity(0.16), radius: 24, x: 0, y: 0)
+    }
+
+    @ViewBuilder
+    func doIf<T: View>(_ condition: Bool, apply: (Self) -> T) -> some View {
+        if condition {
+            apply(self)
+        } else {
+            self
+        }
+    }
 }
